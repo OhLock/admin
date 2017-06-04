48579ec904d268cab7e4762cbc2306f7b549532a
yiiu <yu.gintoki@gmail.com>
2017-06-04 21:40:23 +0800

    updated

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
+import Home from './Home'
+
+const App = () => (
+    <Router>
+        <Route exact path="/" component={ Home }/>
+    </Router>
+)
+export default App
